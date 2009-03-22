e6b3715a429dc0992de124346799333e4f4a1812
David Malcolm <dmalcolm@redhat.com>
2009-03-22 18:38:52 -0400

    Add LGPLv2.1 headers

--- show/tui.py
+++ show/tui.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2009 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU Lesser General Public
+# License, version 2.1 (LGPLv2.1). There is NO WARRANTY for this software,
+# express or implied, including the implied warranties of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. You should have received a copy of
+# LGPLv2.1 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+#
+# Red Hat trademarks are not licensed under LGPLv2.1. No permission is
+# granted to use or replicate Red Hat trademarks that are incorporated in
+# this software or its documentation.
+# 
+# Red Hat Author(s): David Hugh Malcolm <dmalcolm@redhat.com>
+
 import curses
 import table
 
